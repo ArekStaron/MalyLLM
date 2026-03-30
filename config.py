b7405382fbb6df1a204cbe8e0a8ea1c4325329cf
ArekStaron <arkadiusz.staron7@gmail.com>
2026-03-30 15:01:26 +0200

    Adding RoPe and Finish GQA

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ from dataclasses import dataclass
 class SmallTransformerConfig:
     #Model size 
     vocab_size : int = 49152
-    hidden_dim = 576
-    intermediate_dim = 1536
-    num_layer = 30 
-    num_key_value_heads  = 3
-    num_attention_heads = 9
-    head_dim = 64
+    hidden_dim :int = 576
+    intermediate_dim :int= 1536
+    num_layer :int = 30 
+    num_key_value_heads :int  = 3
+    num_attention_heads :int = 9
+    head_dim :int = 64
 
-    max_position_embeddings = 2048 
+    max_position_embeddings :int = 2048 
--- model/model.py
+++ model/model.py
@@ -12,9 +12,12 @@ class Attencion(nn.Module):
 
         self.kv_dim = config.head_dim * config.num_key_value_heads
 
-        self.q_proj = torch.nn.Linear(config.hidden_dim ,  config.hidden_dim  , bias=False)
+        self.q_proj = nn.Linear(config.hidden_dim ,  config.hidden_dim  , bias=False)
         
-        self.kv_proj = torch.nn.Linear(config.hidden_dim , self.kv_dim * 2 , bias= False)
+        self.kv_proj = nn.Linear(config.hidden_dim , self.kv_dim * 2 , bias= False)
+
+        self.c_proj = nn.Linear(config.hidden_dim , config.hidden_dim)
+
 
         self.n_emdb = config.hidden_dim
         self.n_head = config.num_attention_heads
@@ -27,13 +30,30 @@ class Attencion(nn.Module):
         B , T , C = x.shape
 
         kv = self.kv_proj(x)
+        
+
         q = self.q_proj(x)
 
+
         k , v = kv.split(self.kv_dim , dim=-1)
-        
-        q = q.view(B, T ,self.n_head , self.head_dim ).transpose(1,2)
+    
+
+        q = q.view(B, T ,self.n_head , self.head_dim ).transpose(1,2) 
         k = k.view(B,T , self.n_kv_heads , self.head_dim).transpose(1,2)
         v = v.view(B,T , self.n_kv_heads , self.head_dim).transpose(1,2)
 
 
-        y = F.scaled_dot_product_attention()
+        # from (B,n_kv_heads , T , self.head_dim ) --> (B,self.n_head, T , self.head_dim)
+        # For dim to match in k , v and q 
+        n_repats = self.n_head //self.n_kv_heads
+
+        k = k.repeat_interleave(n_repats , dim=1)
+        v = v.repeat_interleave(n_repats , dim=1)
+
+        y = F.scaled_dot_product_attention(q,k,v , is_causal=True) # 
+
+        y = y.transpose(1,2).reshape(B,T,C)
+
+        return y 
+
+        
